e4234c7f5426c2462d0e1a35d8cff28df97f86c0
kagupatipavan <55104400+kagupatipavan@users.noreply.github.com>
2023-02-14 14:56:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("working")
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+    res.send('Hello World');
+    
+})
+app.get('/pa',(req,res)=>{
+    res.send('Hello World all');
+})
+app.listen(3000,52.192.98.115,()=>{
+  console.log("working");
+});
